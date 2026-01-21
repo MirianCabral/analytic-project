86ce6e51f3bbda7a1aa7153a576a92cc3e4b7919
Mirian <mirian.fonseca@aluno.faculdadeimpacta.com.br>
2026-01-21 12:46:03 -0300

    Novas análises

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -33,3 +33,30 @@ SELECT
 FROM fato_vendas
 GROUP BY customer_state
 ORDER BY ticket_medio_estado DESC
+
+--Maior Receita por categoria
+SELECT
+    product_category_name,
+    SUM(receita_item) as receita_categoria
+FROM fato_vendas
+GROUP BY product_category_name
+ORDER BY receita_categoria DESC
+
+--% de pedidos atradados por Mês
+SELECT
+    purchase_year_month,
+    COUNT(DISTINCT order_id) AS total_pedidos,
+    COUNT(DISTINCT CASE WHEN delivery_delay = True THEN order_id END) AS pedidos_atrasados,
+    COUNT(DISTINCT CASE WHEN delivery_delay = True THEN order_id END) * 100 / COUNT(DISTINCT order_id) AS percentual_atraso
+FROM fato_vendas
+GROUP BY purchase_year_month
+ORDER BY purchase_year_month
+
+--Tempo médio de entrega por categoria
+SELECT
+    product_category_name,
+    AVG(delivery_time_days) AS tempo_medio_entrega
+FROM fato_vendas
+WHERE delivery_time_days IS NOT NULL
+GROUP BY product_category_name
+ORDER BY product_category_name
